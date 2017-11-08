8ca44602b4b678052a76f53711a0faaa952e917e
cazz182 <cazz182@gmail.com>
2017-11-08 15:34:10 -0500

    Se adiciono la categoría reflexión

--- app/src/main/java/com/projects/cristianzapata/vaneapp/tabReflexion.java
+++ app/src/main/java/com/projects/cristianzapata/vaneapp/tabReflexion.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 public class tabReflexion extends Fragment {
 
@@ -19,7 +20,19 @@ public class tabReflexion extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reflexion, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_reflexion, container, false);
+
+        // Se obtienen las webView para los textos
+        final WebView wvReflexIntro = (WebView) rootView.findViewById(R.id.introduccionReflexiona);
+        final WebView wvPart1 = (WebView) rootView.findViewById(R.id.parte1Reflexion);
+        final WebView wvPart2 = (WebView) rootView.findViewById(R.id.parte2Reflexion);
+
+        // Se adiciona el texto
+        wvReflexIntro.loadData(getString(R.string.introReflexion), "text/html; charset=utf-8", "utf-8");
+        wvPart1.loadData(getString(R.string.parte1Reflexion), "text/html; charset=utf-8", "utf-8");
+        wvPart2.loadData(getString(R.string.parte2Reflexion), "text/html; charset=utf-8", "utf-8");
+
+        return rootView;
     }
 
 }
