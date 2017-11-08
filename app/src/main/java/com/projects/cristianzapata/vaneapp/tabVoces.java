9bcf6b36f6186e75bd826f76a6b9b1cd0aa4d71d
cazz182 <cazz182@gmail.com>
2017-11-08 11:32:21 -0500

    Se adiciona la categoria voces

--- app/src/main/java/com/projects/cristianzapata/vaneapp/tabVoces.java
+++ app/src/main/java/com/projects/cristianzapata/vaneapp/tabVoces.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 public class tabVoces extends Fragment {
 
@@ -19,6 +20,14 @@ public class tabVoces extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_voces, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_personajes, container, false);
+
+        // Se obtienen las webView para los textos
+        final WebView wvVocesIntro = (WebView) rootView.findViewById(R.id.introduccionVoces);
+
+        // Se adiciona el texto
+        wvVocesIntro.loadData(getString(R.string.introPersonajes), "text/html; charset=utf-8", "utf-8");
+
+        return rootView;
     }
 }
